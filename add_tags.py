6e77083a38fcfa1cd890f5b2f379e3feaed10786
Taka008 <kodama@nlp.ist.i.kyoto-u.ac.jp>
2020-04-21 05:48:38 +0900

    bug fix

--- add_tags.py
+++ add_tags.py
@@ -13,19 +13,23 @@ def main():
                           port=cfg['database']['port'],
                           db_name=cfg['database']['db_name'],
                           collection_name=cfg['database']['collection_name'])
-    for input_path in glob.glob(f"{cfg['crowdsourcing']['result_dir']}/*.jsonl"):
-        with open(input_path) as f:
-            json_tags = [json.loads(line.strip()) for line in f]
-
-        for json_tag in json_tags:
-            page = mongo.collection.find_one({'page.url': json_tag['url']})['page']
-            for tag in TAGS:
-                page[tag] = json_tag['tags'][tag]
-            page['topics'] = json_tag['tags']['topics']
-            mongo.collection.update_one(
-                {'page.url': json_tag['url']},
-                {'$set': {'page': page}}
-            )
+    # for input_path in glob.glob(f"{cfg['crowdsourcing']['result_dir']}/*.jsonl"):
+    #     with open(input_path) as f:
+    #         json_tags = [json.loads(line.strip()) for line in f]
+    #
+    #     for json_tag in json_tags:
+    #         search_result = mongo.collection.find_one({'page.url': json_tag['url']})
+    #         if search_result:
+    #             page = search_result['page']
+    #             for tag in TAGS:
+    #                 page[tag] = json_tag['tags'][tag]
+    #             page['topics'] = json_tag['tags']['topics']
+    #             mongo.collection.update_one(
+    #                 {'page.url': json_tag['url']},
+    #                 {'$set': {'page': page}}
+    #             )
+    docs = [doc for doc in mongo.get_filtered_pages('all')]
+    print(len(docs))
 
 
 if __name__ == '__main__':
