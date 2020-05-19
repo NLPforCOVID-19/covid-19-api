857330dd4dd37f77050814a6a5e7f073c36accd8
Taka008 <kodama@nlp.ist.i.kyoto-u.ac.jp>
2020-05-19 17:30:05 +0900

    refactoring

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 import os
 import json
-import glob
 import logging
 import copy
 
@@ -75,11 +74,16 @@ class DBHandler:
             "is_clear": is_clear,
             "is_about_false_rumor": is_about_false_rumor
         }
-        self.collection.update_one(
-            {"page.url": url},
-            {"$set": {"page": document_}},
-            upsert=True
-        )
+
+        existing_page = self.collection.find_one({"page.url": url})
+        if existing_page and orig["timestamp"] > existing_page["page"]["orig"]["timestamp"]:
+            self.collection.update_one(
+                {"page.url": url},
+                {"$set": {"page": document_}},
+                upsert=True
+            )
+        elif not existing_page:
+            self.collection.insert_one({"page": document_})
 
     @staticmethod
     def _reshape_page(page: dict) -> dict:
@@ -211,30 +215,29 @@ def main():
 
     # reflect the crowdsourcing results
     if os.path.isdir(cfg["crowdsourcing"]["result_dir"]):
-        for input_path in glob.glob(f'{cfg["crowdsourcing"]["result_dir"]}/*.jsonl'):
-            with open(input_path) as f:
-                json_tags = [json.loads(line.strip()) for line in f]
+        with open(f'{cfg["crowdsourcing"]["result_dir"]}/crowdsourcing_all.jsonl') as f:
+            json_tags = [json.loads(line.strip()) for line in f]
 
-            for json_tag in json_tags:
-                search_result = mongo.collection.find_one({"page.url": json_tag["url"]})
-                if search_result:
-                    page = search_result["page"]
-                    page["is_checked"] = 1
-                    for tag in TAGS:
-                        page[tag] = json_tag["tags"][tag]
+        for json_tag in json_tags:
+            search_result = mongo.collection.find_one({"page.url": json_tag["url"]})
+            if search_result:
+                page = search_result["page"]
+                page["is_checked"] = 1
+                for tag in TAGS:
+                    page[tag] = json_tag["tags"][tag]
 
-                    new_topics = [topic for topic, has_topic in json_tag["tags"]["topics"].items() if has_topic]
-                    page["topics"] = new_topics
+                new_topics = [topic for topic, has_topic in json_tag["tags"]["topics"].items() if has_topic]
+                page["topics"] = new_topics
 
-                    old_snippets = page["snippets"]
-                    new_snippets = {}
-                    for new_topic in new_topics:
-                        new_snippets[new_topic] = old_snippets[new_topic] if new_topic in old_snippets.keys() else ""
+                old_snippets = page["snippets"]
+                new_snippets = {}
+                for new_topic in new_topics:
+                    new_snippets[new_topic] = old_snippets[new_topic] if new_topic in old_snippets.keys() else ""
 
-                    mongo.collection.update_one(
-                        {"page.url": json_tag["url"]},
-                        {"$set": {"page": page}}
-                    )
+                mongo.collection.update_one(
+                    {"page.url": json_tag["url"]},
+                    {"$set": {"page": page}}
+                )
 
 
 if __name__ == "__main__":
