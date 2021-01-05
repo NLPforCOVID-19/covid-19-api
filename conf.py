026a21deeaf8b6b56d18fc8cc567069944229826
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2021-01-05 17:23:52 +0900

    clean

--- conf.py
+++ conf.py
@@ -1,44 +1,44 @@
 import os
 import json
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 config = {
     'activator': os.getenv('ACTIVATOR'),
-    'access_control_allow_origin': '*',
-    'database': {
-        'input_page_path': os.getenv('DB_INPUT_PAGE_PATH'),
-        'host': os.getenv('DB_HOST'),
-        'port': int(os.getenv('DB_PORT')),
-        'db_name': os.getenv('DB_DB_NAME'),
-        'collection_name': os.getenv('DB_COLLECTION_NAME'),
-        'log_path': os.getenv('DB_UPDATE_LOG_PATH'),
-        'category_check_log_path': os.getenv('DB_CATEGORY_CHECK_LOG_PATH')
-    },
-    'es': {
-        'host': os.getenv('ES_HOST'),
-        'port': int(os.getenv('ES_PORT'))
-    },
-    'crowdsourcing': {
-        'result_dir': os.getenv('CS_RESULT_DIR')
-    },
-    'feedback': {
-        'slack': [
-            {
-                'access_token': access_token,
-                'channel': channel,
-            }
-            for access_token, channel
-            in zip(os.getenv('SLACK_ACCESS_TOKENS').split(' '), os.getenv('SLACK_APP_CHANNELS').split(' '))
-        ],
-        'feedback_log_file': os.getenv('FB_LOG_FILE')
-    },
-    'source': os.getenv('SOURCE'),
     'password': os.getenv('PASSWORD'),
-    'webcite_url': os.getenv('WEBSITE_URL'),
-    'twitter': {
-        'api_key': os.getenv('TWITTER_API_KEY'),
-        'api_secret_key': os.getenv('TWITTER_API_SECRET_KEY'),
-        'token': os.getenv('TWITTER_TOKEN'),
-        'secret_token': os.getenv('TWITTER_SECRET_TOKEN'),
+    'log_handler': {
+        'log_dir': os.getenv('LOG_HANDLER_LOG_DIR'),
+    },
+    'cors': {
+        'origins': os.getenv('CORS_ORIGINS'),
+    },
+    'db_handler': {
+        'mongo_host': os.getenv('DB_HANDLER_MONGO_HOST'),
+        'mongo_port': int(os.getenv('DB_HANDLER_MONGO_PORT')),
+        'mongo_db_name': os.getenv('DB_HANDLER_MONGO_DB_NAME'),
+        'mongo_collection_name': os.getenv('DB_HANDLER_MONGO_COLLECTION_NAME'),
+        'es_host': os.getenv('DB_HANDLER_ES_HOST'),
+        'es_port': int(os.getenv('DB_HANDLER_ES_PORT'))
+    },
+    'twitter_handler': {
+        'token': os.getenv('TWITTER_HANDLER_OAUTH_TOKEN'),
+        'token_secret': os.getenv('TWITTER_HANDLER_OAUTH_TOKEN_SECRET'),
+        'consumer_key': os.getenv('TWITTER_HANDLER_OAUTH_CONSUMER_KEY'),
+        'consumer_secret': os.getenv('TWITTER_HANDLER_OAUTH_CONSUMER_SECRET'),
+    },
+    'slack_handlers': [
+        {
+            'access_token': access_token,
+            'app_channel': app_channel
+        }
+        for access_token, app_channel
+        in zip(os.getenv('SLACK_HANDLER_ACCESS_TOKENS').split(), os.getenv('SLACK_HANDLER_APP_CHANNELS').split())
+    ],
+    'data': {
+        'article_list': os.getenv('ARTICLE_LIST'),
+        'site_list': os.getenv('SITE_LIST')
     }
 }
 
