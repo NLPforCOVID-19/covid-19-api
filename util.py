bdf78520995da523d804472f58e991b8d032483c
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2021-05-13 19:04:01 +0900

    add ru

--- util.py
+++ util.py
@@ -147,7 +147,7 @@ ECOUNTRY_ICOUNTRIES_MAP = {
     'jp': ['jp'],
     'cn': ['cn'],
     'us': ['us', 'us_other'],
-    'eur': ['eur', 'eu', 'fr', 'gb', 'es', 'de', 'eur_other'],
+    'eur': ['eur', 'eu', 'fr', 'gb', 'es', 'de', 'ru', 'eur_other'],
     'asia': ['asia', 'kr', 'id', 'in', 'np', 'my', 'sg', 'asia_other'],
     'sa': ['sa', 'br', 'mx', 'sa_other'],
     'oceania': ['au', 'oceania_other'],
