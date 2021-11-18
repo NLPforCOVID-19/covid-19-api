86596301c1065b9d3e51d9daf3e6f2ff0af3207d
Taka008 <kodama@nlp.ist.i.kyoto-u.ac.jp>
2021-11-18 16:17:18 +0900

    remove infection state news from tweet candidates

--- cron.py
+++ cron.py
@@ -173,7 +173,7 @@ def update_database(do_tweet: bool = False):
                     "is_positive": is_positive
                 }
             )
-            if r and do_tweet and r["status"] == Status.INSERTED and r["is_positive"]:
+            if r and do_tweet and r["status"] == Status.INSERTED and r["is_positive"] and "感染状況" not in topics:
                 maybe_tweeted_ds.append(r)
         line_num = line_idx
     with open(cache_file, "w") as f:
